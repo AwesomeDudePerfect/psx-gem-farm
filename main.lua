94dc9cda5966b14fd425e4b443555775ffe635b1
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2024-01-17 03:09:03 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,10 +4,4 @@ getgenv().AuthKey = "HUGE_gmqREZqn7kKB"
 getgenv().LoadSettings = {
     Example_Setting = Example_Value
 }
-local success, error = loadstring(game:HttpGet("https://raw.githubusercontent.com/AwesomeDudePerfect/psx-gem-farm/main/6a4a1944b1f257b9e52e2fe387345ab2.lua"))()
-wait(15)
-if success then
-    print('executed')
-else
-    loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AwesomeDudePerfect/psx-gem-farm/main/6a4a1944b1f257b9e52e2fe387345ab2.lua"))()
