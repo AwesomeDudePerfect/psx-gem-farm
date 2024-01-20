adc9e8d3bebf1a334f9a152437635f9206d074b2
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2024-01-21 02:09:32 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,4 +4,4 @@ getgenv().AuthKey = "HUGE_gmqREZqn7kKB"
 getgenv().LoadSettings = {
     Example_Setting = Example_Value
 }
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AwesomeDudePerfect/psx-gem-farm/main/6a4a1944b1f257b9e52e2fe387345ab2.lua"))()
+loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
