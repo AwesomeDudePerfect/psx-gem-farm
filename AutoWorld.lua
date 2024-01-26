d212de9ae367e7be45353d0f781697f8925d88d0
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2024-01-27 01:57:39 +0800

    Add files via upload

---
+++ AutoWorld.lua
@@ -0,0 +1,110 @@
+wait(10)
+local savemodule = require(game:GetService("ReplicatedStorage").Library.Client.Save)
+local SaveFile = savemodule.Get(game.Players.LocalPlayer)
+local UnlockedAreas = SaveFile.UnlockedZones
+
+local lplr = game:GetService("Players").LocalPlayer
+local Character = lplr.Character or lplr.CharacterAdded:Wait()
+local HRP = Character:WaitForChild("HumanoidRootPart")
+local Enabled = true
+local Mouse = lplr:GetMouse()
+local MapContainer = workspace:WaitForChild("Map")
+local AreaModules = game:GetService("ReplicatedStorage"):WaitForChild("__DIRECTORY"):WaitForChild("Zones")
+local AreaUnlocker = game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Zones_RequestPurchase")
+local CurrentArea = 0 -- index to get next area
+local AreaToUnlock = ""
+local FieldPart = nil -- instance
+local AreaList = {}
+
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+    wait(1)
+    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+warn("Everything has loaded fully. Enjoy :)")
+
+-- grab new hrp
+local c,c2
+-- enable/disable
+c2 = Mouse.KeyDown:Connect(function(Key)
+    if Key == "p" then Enabled = not Enabled end
+end)
+
+-- get list of areas
+for _,v in pairs(AreaModules:GetDescendants()) do
+    if not v:IsA("ModuleScript") then print('nah') end
+    local Info = string.split(v.Name," | ")
+    AreaList[tonumber(Info[1])] = Info[2]
+end
+
+------ AUTO GET ORBS/LOOTBAGS
+local OrbRemote = game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Orbs: Collect")
+local LootbagRemote = game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Lootbags_Claim")
+local OrbFolder = workspace:WaitForChild("__THINGS"):WaitForChild("Orbs")
+local LootbagFolder = workspace:WaitForChild("__THINGS"):WaitForChild("Lootbags")
+local autoOrbConnection, autoLootBagConnection
+
+autoOrbConnection = OrbFolder.ChildAdded:Connect(function(v)
+    OrbRemote:FireServer({tonumber(v.Name)})
+    task.wait()
+    v:Destroy()
+end)
+
+autoLootBagConnection = LootbagFolder.ChildAdded:Connect(function(v)
+    LootbagRemote:FireServer({v.Name})
+    task.wait()
+    v:Destroy()
+end)
+-------------------
+
+c = lplr.CharacterAdded:Connect(function(Char)
+    Character = Char
+    HRP = Character:WaitForChild("HumanoidRootPart")
+end)
+
+local function Unlock()
+    return AreaUnlocker:InvokeServer(AreaToUnlock)
+end
+
+-- find current area
+for Area,_ in pairs(UnlockedAreas) do
+    local AreaNum = table.find(AreaList,Area)
+    if AreaNum > CurrentArea then
+        CurrentArea = AreaNum
+        AreaToUnlock = AreaList[AreaNum+1]
+        FieldPart = MapContainer:WaitForChild(AreaNum.." | "..Area):WaitForChild("INTERACT"):WaitForChild("BREAK_ZONES"):WaitForChild("BREAK_ZONE")
+        HRP.CFrame = FieldPart.CFrame
+        task.wait(.2) -- wait for the game to load in everything
+        --print("new greatest area",CurrentArea,AreaToUnlock)
+    end
+end
+
+print("area to unlock:", AreaToUnlock)
+
+while true do
+    if Enabled then
+        -- attempt buy new area
+        if Unlock() then -- unlock succeeded
+            task.wait(3)
+            CurrentArea = CurrentArea + 1
+            AreaToUnlock = AreaList[CurrentArea+1]
+            FieldPart = MapContainer:WaitForChild(CurrentArea.." | "..AreaList[CurrentArea]):WaitForChild("INTERACT"):WaitForChild("BREAK_ZONES"):WaitForChild("BREAK_ZONE")
+            HRP.CFrame = FieldPart.CFrame
+            
+            -- FOR DESIRED STOPPING POINTS
+            if CurrentArea == 92 then
+                print("DESIRED AREA UNLOCKED")
+                break
+            end
+        else
+            HRP.CFrame = FieldPart.CFrame
+        end
+    else
+        break
+    end
+    task.wait(15)
+end
+
+c:Disconnect()
+c2:Disconnect()
