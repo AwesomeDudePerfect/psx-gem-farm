c38c8cb310cc05dc800aa0eb35762b9f7ae3fb53
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2024-01-27 22:40:05 +0800

    Update fish.lua

--- fish.lua
+++ fish.lua
@@ -88,7 +88,35 @@ local function getMonkeyBubbles(anchor)
 end
 
 local function jumpToServer()
-    -- Implementation remains the same
+	repeat
+		local deep = math.random(1, 5)
+		local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s" 
+		local req = request({ Url = string.format(sfUrl, 8737899170, "Asc", 100) }) 
+		local body = http:JSONDecode(req.Body)
+		
+		if deep > 1 then
+	        for i = 1, deep, 1 do 
+	         	req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 8737899170, "Asc", 100) }) 
+	         	body = http:JSONDecode(req.Body) 
+	        	task.wait(0.1)
+	        end
+		end
+	
+	    local servers = {}
+	    if body and body.data then
+	        for i, v in next, body.data do
+	    	    if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < 10 and v.id ~= game.JobId then
+	            	table.insert(servers, v.id)
+	        	end
+	        end
+	    end
+	
+	    local randomCount = #servers
+	    if not randomCount then
+			randomCount = 2
+	    end
+    	ts:TeleportToPlaceInstance(8737899170, servers[math.random(1, randomCount)], game:GetService("Players").LocalPlayer)
+	until game.JobId ~= game.JobId
 end
 
 -- Anti-AFK
