238bdbe34bd88e174aec4a21e878cd80d58aee4b
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2024-01-27 16:11:54 +0800

    Update fish.lua

--- fish.lua
+++ fish.lua
@@ -28,6 +28,8 @@ local function teleportToFishingSite()
     game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Teleports_RequestTeleport"):InvokeServer(unpack({[1] = "Cloud Forest"}))
     wait(10)
     local tpAdvancedFishing = game:GetService("Workspace"):WaitForChild("__THINGS"):WaitForChild("Instances"):WaitForChild("AdvancedFishing").Teleports.Enter
+    print(HRP.CFrame)
+    print(tpAdvancedFishing.CFrame)
     HRP.CFrame = tpAdvancedFishing.CFrame
 end
 
