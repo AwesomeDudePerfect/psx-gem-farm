fc1ba0803e0eff075c77988798b38e0119faaebb
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2024-01-27 21:51:47 +0800

    Update fish.lua

--- fish.lua
+++ fish.lua
@@ -91,11 +91,53 @@ local function getMonkeyBubbles(anchor)
     return myBubbles
 end
 
+local function jumpToServer()
+	repeat
+		local deep = math.random(1, 5)
+		local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s" 
+		local req = request({ Url = string.format(sfUrl, 8737899170, "Asc", 100) }) 
+		local body = http:JSONDecode(req.Body)
+		
+		if deep > 1 then
+	        for i = 1, deep, 1 do 
+	         	req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 8737899170, "Asc", 100) }) 
+	         	body = http:JSONDecode(req.Body) 
+	        	task.wait(0.1)
+	        end
+		end
+	
+	    local servers = {}
+	    if body and body.data then
+	        for i, v in next, body.data do
+	    	    if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < 10 and v.id ~= game.JobId then
+	            	table.insert(servers, v.id)
+	        	end
+	        end
+	    end
+	
+	    local randomCount = #servers
+	    if not randomCount then
+			randomCount = 2
+	    end
+    	ts:TeleportToPlaceInstance(8737899170, servers[math.random(1, randomCount)], game:GetService("Players").LocalPlayer)
+	until game.JobId ~= game.JobId
+end
+
 --anti afk shit
 game:GetService("Players").LocalPlayer.PlayerScripts.Scripts.Core["Idle Tracking"].Disabled = true
 
 --low cpu nigga optimizer
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AwesomeDudePerfect/psx-gem-farm/main/lowCpu.lua"))()
+
+--auto recon
+task.spawn(function()
+    game:GetService("GuiService").ErrorMessageChanged:Connect(function()
+        jumpToServer() -- call your tp function here
+        game.Players.LocalPlayer:Kick("Found An Error, Reconnecting...")
+        print("Found An Error, Reonnecting...")
+        wait(0.1)
+    end)
+end)
 
 while task.wait(1) do
     pcall(function()
