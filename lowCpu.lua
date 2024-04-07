6f2487fd9a6c8ff97fd95da33997e0136150a5e3
AwesomeDudePerfect <92684391+AwesomeDudePerfect@users.noreply.github.com>
2024-04-07 18:42:09 +0800

    Update lowCpu.lua

--- lowCpu.lua
+++ lowCpu.lua
@@ -1,39 +1,61 @@
-repeat wait(1) until game:IsLoaded()
-local Lib = require(game.ReplicatedStorage.Library)
+repeat
+    task.wait()
+until game:IsLoaded()
 
-function xTab(TABLE)
-    for i,v in pairs(TABLE) do
-        if type(v) == "function" then
-            TABLE[i] = function(...) return end
-        end
-        if type(v) == "table" then
-            xTab(v)
-        end
-    end
-end
-xTab(Lib.WorldFX)
-xTab(Lib.NotificationCmds.Item)
+local Workspace = game:GetService("Workspace")
+local Terrain = Workspace:WaitForChild("Terrain")
+Terrain.WaterReflectance = 0
+Terrain.WaterTransparency = 1
+Terrain.WaterWaveSize = 0
+Terrain.WaterWaveSpeed = 0
 
-for i,v in pairs(game:GetDescendants()) do
-    if v:IsA("MeshPart") then
-        v.MeshId = ""
-    end
-    if v:IsA("BasePart") or v:IsA("MeshPart") then
+local Lighting = game:GetService("Lighting")
+Lighting.Brightness = 0
+Lighting.GlobalShadows = false
+Lighting.FogEnd = 9e100
+Lighting.FogStart = 0
+
+sethiddenproperty(Lighting, "Technology", 2)
+sethiddenproperty(Terrain, "Decoration", false)
+
+local function clearTextures(v)
+    if v:IsA("BasePart") and not v:IsA("MeshPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+    elseif (v:IsA("Decal") or v:IsA("Texture")) then
         v.Transparency = 1
-    end
-    if v:IsA("Texture") or v:IsA("Decal") then
-        v.Texture = ""
-    end
-    if v:IsA("ParticleEmitter") then
+    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
         v.Lifetime = NumberRange.new(0)
-        v.Transparency = NumberSequence.new({NumberSequenceKeypoint.new(0,0), NumberSequenceKeypoint.new(1,0)})
+    elseif v:IsA("Explosion") then
+        v.BlastPressure = 1
+        v.BlastRadius = 1
+    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
         v.Enabled = false
-    end
-    if v:IsA("BillboardGui") or v:IsA("SurfaceGui") or v:IsA("Trail") or v:IsA("Beam") then
-        v.Enabled = false
-    end
-    if v:IsA("Highlight") then
-        v.OutlineTransparency = 1
-        v.FillTransparency = 1
+    elseif v:IsA("MeshPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+        v.TextureID = 10385902758728957
+    elseif v:IsA("SpecialMesh")  then
+        v.TextureId = 0
+    elseif v:IsA("ShirtGraphic") then
+        v.Graphic = 1
+    elseif (v:IsA("Shirt") or v:IsA("Pants")) then
+        v[v.ClassName .. "Template"] = 1
+    elseif v.Name == "Foilage" and v:IsA("Folder") then
+        v:Destroy()
+    elseif string.find(v.Name, "Tree") or string.find(v.Name, "Water") or string.find(v.Name, "Bush") or string.find(v.Name, "grass") then
+        task.wait()
+        v:Destroy()
     end
 end
+
+setfpscap(15)
+game:GetService("Lighting"):ClearAllChildren()
+
+for _, v in pairs(Workspace:GetDescendants()) do
+    clearTextures(v)
+end
+
+Workspace.DescendantAdded:Connect(function(v)
+    clearTextures(v)
+end)
